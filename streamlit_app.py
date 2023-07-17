1f2a8170859d05268daef3fcd5313e2961319ce0
memoran70 <139807638+memoran70@users.noreply.github.com>
2023-07-17 12:17:11 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
-streamlit.header('Breakfast Menu')
+streamlit.header('Breakfast Favorites')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach and Rocket Smoothie')
-streamlit.text('Hard-boiled Free-Range Egg')
+streamlit.text('🥚 Hard-boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
